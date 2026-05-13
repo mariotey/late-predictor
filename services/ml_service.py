289d0e4bc986218585e763c98825f30f71f1a168
mctey <tey.mingchuan@yahoo.com.sg>
2026-05-14 03:07:39 +0800

    Updated notebooks

--- services/ml_service.py
+++ services/ml_service.py
@@ -53,7 +53,7 @@ class MLService:
             logger.error(f"Prediction failed: {e}")
             raise ValueError("Model not trained!")
 
-        result = {"pred_min": float(pred)}
+        result = {"est_min": float(pred)}
 
         logger.info(f"Output:\n{result}")
 
